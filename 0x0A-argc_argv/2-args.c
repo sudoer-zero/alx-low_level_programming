26eec55a039acb181b482b82a346ef95cf88e8f1
Sudoer <sudoer.zero@gmail.com>
2022-11-05 07:21:30 +0200

    Update 2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,45 @@
-#include <stdio.h>
 #include "main.h"
+
+void print_array_elements(char *str);
+
 /**
- * main - prints the arguments passed followed by a newline
- * @argc: command line argument count
- * @argv: command line argument vector
- *
- * Return: Always 0
- */
+* main - Prints all arguments it receives
+*
+* @argc: Length of @argv, integer
+*
+* @argv: Array of strings of the arguments of this program
+*
+* Return: 0, success
+*/
+
 int main(int argc, char *argv[])
 {
 	int i;
 
-	for(i=0; i<argc; i++)
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[i]);
+		print_array_elements(argv[i]);
+		_putchar('\n');
 	}
 
 	return (0);
+}
+
+/**
+* print_array_elements - Prints all char of a string
+*
+* @str: Pointer to string
+*
+* Return: void
+*/
+
+void print_array_elements(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 }
