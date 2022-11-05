8eb965a881a1ed457170bf0d549ca31f12f9ea2c
Sudoer <sudoer.zero@gmail.com>
2022-11-05 07:00:08 +0200

    The best argument againt democracy is a five-minute

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the arguments passed followed by a newline
+ * @argc: command line argument count
+ * @argv: command line argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i=0; i<argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
